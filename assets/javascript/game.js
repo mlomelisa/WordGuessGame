b251c4e9de4ee6f09183e46ed4f62e242708c697
mlomelisa <monica.lomelim@gmail.com>
2019-06-10 16:33:22 -0700

    Added clean process and loose process

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,89 +7,104 @@ let totalletterChoose = document.getElementById('totalLetter');
 let totalTurns = document.getElementById('total-turns');
 let startButton = document.getElementById('startButton');
 
+let answerArray = [];
+let totalletterArray = [];
+let remainLetters = 0;
+
+
 // Array of words
 let wordsArray = ['tokyo','opening','medal','swimming'];
 
 // Pick a random word from the array
 
- let word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
- let answerArray = [];
- let totalletterArray = [];
- let remainLetters = 0;
+function getWord(word) {
 
- function main() {
-  startButton.addEventListener('click', () => {
-    getWord();  
-    resultMessage.innerHTML = ' Guess a letter!!';
-  })
- };
-
-function getWord() { 
   for (let i = 0; i < word.length; i++) {
     answerArray[i] = '_';
     letterspace.textContent = answerArray.join(" ");
     }
+    remainGuess(word);
     console.log(word);
-   remainGuess();
+ }
+
+ function remainGuess(word){
+  remainLetters = word.length;
+  totalTurns.innerHTML = remainLetters;
+  return remainLetters;
+}
+
+function game(userGuess, word) {  
+  for (let j = 0; j < word.length; j++ ) {
+    if (word[j] === userGuess) {
+      answerArray[j] = userGuess;
+      letterspace.innerHTML = answerArray.join(" ");          
+     }        
+  }    
+}
+
+
+function main() {
+    startButton.addEventListener('click', () => {
+    resultMessage.innerHTML = 'Guess a letter!!'; 
+    let word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    totalletterArray = [];
+    totalletterChoose.innerHTML = totalletterArray;
+    answerArray = [];
+    getWord(word);
+    remainGuess(word);
    
-}
-
-function remainGuess(){
-   remainLetters = word.length;
-   totalTurns.innerHTML = remainLetters;
-   return remainLetters;
-}
-
-
-document.onkeyup = function(event) {
-    let userGuess = event.key;
-      
-      
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-        console.log(event.keyCode);
-
+  
+    document.onkeyup = function(event) {
+      let userGuess = event.key;
         
-    
-        if ( totalletterArray.includes(userGuess) ){
-          resultMessage.innerHTML = 'Already choose that letter!!';
-        } else {
-          if( word.includes(userGuess)) {
-            console.log(userGuess);
-            resultMessage.innerHTML = 'Good work!!';
-            game(userGuess);
-          } else {
-            resultMessage.innerHTML = 'Upss you lose a point!!';
-            remainLetters--;
-            totalTurns.innerHTML = remainLetters;
-          }
-          totalletterArray.push(userGuess);
-
-          totalletterChoose.innerHTML = totalletterArray;
-        
-          console.log(totalletterArray);
+      if (event.keyCode >= 65 && event.keyCode <= 90) {
+        evualtion(userGuess, word) 
+            
         }
-       
-      } else {
-        resultMessage.innerHTML = 'Please enter only alphabets in lower cases!!';
-      }
-}
-
-  // Function to fill the letter found in the word array
-  function game(userGuess) {
-  
-    for (let j = 0; j < word.length; j++ ) {
-      if (word[j] === userGuess) {
-        answerArray[j] = userGuess;
-        letterspace.innerHTML = answerArray.join(" ");
-        console.log(answerArray);       
-       }        
-    }    
+        else {
+          resultMessage.innerHTML = 'Please enter only alphabets in lower cases!!';
+        }  
   }
+});
+};
+function evualtion(userGuess, word) {
 
+  if ( totalletterArray.includes(userGuess) ){
+    resultMessage.innerHTML = 'Already choose that letter!!';
+    } 
+    else {
+         if( word.includes(userGuess)) {
+           console.log(userGuess);
+           resultMessage.innerHTML = 'Good work!!';
+           game(userGuess,word);
+           totalletterArray.push(userGuess);
+         } else {
+           resultMessage.innerHTML = 'Upss you lose a point!!';
+           
+           if ( !word.includes(userGuess) ) {
+             remainLetters--;
+             totalTurns.innerHTML = remainLetters;
+             totalletterArray.push(userGuess);
+             verify();   
+           }
+         }
+         totalletterChoose.innerHTML = totalletterArray;   
+     } 
+    };
+  
+  function verify(){
+    if (remainLetters > 0){
+
+    } else{
+    
+      resultMessage.innerHTML = 'Game Over mmmm!!'
+      setTimeout( clean,  2000);
+     }
+    }
+
+    function clean() {
+      document.getElementById('startButton').click();
+      }
+ 
+ 
 main();
-
-
-  
-  
-
-
